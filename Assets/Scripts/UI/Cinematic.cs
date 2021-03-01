022b6ee3a7d99696baad1d96644adc2ad626cf07
LesterTremens <orapro@me.com>
2021-02-28 22:20:13 -0600

    Agregando UI

---
+++ Assets/Scripts/UI/Cinematic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cinematic : MonoBehaviour
+{
+    int number = 0;
+    float secondsCounter = 0;
+    float secondsToCount = 1;
+    public Text cinematic;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondsCounter += Time.deltaTime;
+        if (secondsCounter >= secondsToCount)
+        {
+            
+            secondsCounter = 0;
+            number++;
+        }
+        cinematic.text = number.ToString();
+
+        if (number == 5)
+        {
+            SceneManager.LoadScene("PrincipalScreen",LoadSceneMode.Single);
+        }
+    }
+}
